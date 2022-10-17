8ba3081d1760c809b3fc9eac71ed34a3bce49dc2
cah <bani0000@naver.com>
2022-10-17 12:32:42 +0900

    모듈 함수를 이용한 강의(end)

--- test.py
+++ test.py
@@ -3,14 +3,12 @@ import mod1
 num1 = 10
 num2 = 5
 
-#더하기를 해보자
-num1+num2
+#더하기를 해보자 num1+num2
 result1 = mod1.add(num1, num2)
 #결과값 출력을 해보자
 print(result1)
 
-#빼기를 해보자
-num1-num2
+#빼기를 해보자 num1-num2
 result2 = mod1.sub(num1, num2)
 #결과값 출력을 해보자
 print(result1)
@@ -45,9 +43,10 @@ munustep = '''
 #두 수를 입력 받게 만들고
 num1 = int(input("첫번째 수 입력:")) #문자로 입력 되기 때문에 숫자로 변경
 num2 = int(input("첫번째 수 입력:")) #문자로 입력 되기 때문에 숫자로 변경
+
 #메뉴 1.더하기 2.빼기
-print(1.더하기 2.빼기)
-num = int(input("선택"))
+print("1.더하기 2.빼기")
+num = int(input("선택: "))
 
 #만약에 1를 누르면 두 수의 뺸 결과값
 if num == 1:
